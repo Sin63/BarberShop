6f2f85c599d99f4a220a3c87f97cff074d37d4a1
SERGEY <sincov63@gmail.com>
2020-05-24 13:54:46 +0300

    Update Gemfile

--- Gemfile
+++ Gemfile
@@ -1,3 +1,12 @@
 source "https://rubygems.org"
 
 gem "sinatra"
+gem "sinatra-activerecord"
+gem "sqlite3"
+gem "rake"
+gem "activerecord"
+gem "sinatra-contrib"
+
+group :development do 
+    gem "tux"
+end
--- app.rb
+++ app.rb
@@ -1,7 +1,8 @@
-#encoding: utf-8
+require "sinatra/activerecord"
 require 'rubygems'
 require 'sinatra'
 require 'sinatra/reloader'
+require ''
 
 get '/' do
 	erb "Hello! <a href=\"https://github.com/bootstrap-ruby/sinatra-bootstrap\">Original</a> pattern has been modified for <a href=\"http://rubyschool.us/\">Ruby School</a>"			
