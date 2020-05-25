37bf3bcc1f29f18742139250856c3758c09048c7
SERGEY <sincov63@gmail.com>
2020-05-25 17:12:21 +0300

    Add length and numericality validation

--- app.rb
+++ app.rb
@@ -6,7 +6,7 @@ require 'sinatra/reloader'
 set :database, {adapter: "sqlite3", database: "barbershop.db"}
 
 class Client < ActiveRecord::Base
-	validates :name, presence: true
+	validates :name, presence: true, length: { minimum: 3 }, numericality: true
 	validates :phone, presence: true
 	validates :datestamp, presence: true
 	validates :color, presence: true
